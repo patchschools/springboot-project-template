95bcb484f60b28ddc3db644b58fb70854f3c627e
Naresh Kumar H <nareshkumarh@live.com>
2022-01-23 13:58:38 +0530

    Disabled datasource

--- src/main/java/in/spinsoft/demoapp/Application.java
+++ src/main/java/in/spinsoft/demoapp/Application.java
@@ -2,8 +2,10 @@ package in.spinsoft.demoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication//(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class Application {
 
 	public static void main(String[] args) {
